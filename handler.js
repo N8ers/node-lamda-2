865c00c1a7990bfe8ab801f7c08a2e4695b8d6c1
N8ers <nathanraysheryak@gmail.com>
2018-09-03 08:00:37 -0400

    wrapped up review

--- handler.js
+++ handler.js
@@ -4,7 +4,20 @@ module.exports.hello = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: 'I am connected to the server',
+      input: event,
+    }),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.imageResize = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'imageResize connected to server',
       input: event,
     }),
   };
